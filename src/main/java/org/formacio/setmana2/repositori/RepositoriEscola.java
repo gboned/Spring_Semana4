2cd432806141fda06b10643b2579f6c023fef3ec
gboned <gboned@cifpfbmoll.eu>
2020-06-07 23:19:09 +0200

    feat(RepositoriEscola): Implementar método apunta

--- src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
+++ src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
@@ -3,6 +3,7 @@ package org.formacio.setmana2.repositori;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.formacio.setmana2.domini.Alumne;
 import org.formacio.setmana2.domini.Curs;
 import org.formacio.setmana2.domini.Matricula;
 import org.springframework.stereotype.Service;
@@ -16,15 +17,17 @@ public class RepositoriEscola {
 
 	@PersistenceContext
 	private EntityManager em;
-
 	
 	public Curs carregaCurs(String nom) {
 		return em.find(Curs.class, nom);
 	}
-	
-	
+
 	public Matricula apunta (String alumne, String curs) throws EdatIncorrecteException {
-	    return null;	
+		Matricula nova = new Matricula();
+		nova.setAlumne(em.find(Alumne.class, alumne));
+		nova.setCurs(em.find(Curs.class, curs));
+		em.persist(nova);
+		return nova;
 	}
 	
 	
