ba13d08baae4f786693c336536890e743c977eee
gboned <gboned@cifpfbmoll.eu>
2020-06-07 14:05:47 +0200

    feat(Matricula): Mapear entidad y sus relaciones con otras

--- src/main/java/org/formacio/setmana2/domini/Matricula.java
+++ src/main/java/org/formacio/setmana2/domini/Matricula.java
@@ -1,12 +1,23 @@
 package org.formacio.setmana2.domini;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="T_MATRICULES")
 public class Matricula {
-
+	@Id
+	@Column(name="MAT_ID")
 	private Long id; 
-	
+	@ManyToOne
+	@JoinColumn(name="MAT_ALUMNE", referencedColumnName="ALU_NOM")
 	private Alumne alumne;
-	
+	@ManyToOne
+	@JoinColumn(name="MAT_CURS", referencedColumnName="CUR_NOM")
 	private Curs curs;
 	
 	public Long getId() {
