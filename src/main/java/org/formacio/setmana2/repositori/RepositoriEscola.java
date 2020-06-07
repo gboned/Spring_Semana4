45f94544a42d3eb2ede388223d9713aa11820c3f
gboned <gboned@cifpfbmoll.eu>
2020-06-07 17:12:54 +0200

    feat(RepositoriEscola): Convertir en componente

--- src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
+++ src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
@@ -2,11 +2,13 @@ package org.formacio.setmana2.repositori;
 
 import org.formacio.setmana2.domini.Curs;
 import org.formacio.setmana2.domini.Matricula;
+import org.springframework.stereotype.Service;
 
 /**
  * Modifica aquesta classe per tal que sigui un component Spring que realitza les 
  * operacions de persistencia tal com indiquen les firmes dels metodes
  */
+@Service
 public class RepositoriEscola {
 
 	
