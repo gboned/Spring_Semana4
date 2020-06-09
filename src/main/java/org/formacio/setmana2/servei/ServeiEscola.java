1f390b47a47c60f208f1224ff976db8eef8f898e
gboned <gboned@cifpfbmoll.eu>
2020-06-09 11:39:50 +0200

    feat(ServeiEscola): Añadir contenido método apunta y añadir anotación Transactional para pasar dos últimos casos test

--- src/main/java/org/formacio/setmana2/servei/ServeiEscola.java
+++ src/main/java/org/formacio/setmana2/servei/ServeiEscola.java
@@ -1,21 +1,31 @@
 package org.formacio.setmana2.servei;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.formacio.setmana2.domini.Matricula;
 import org.formacio.setmana2.repositori.EdatIncorrecteException;
+import org.formacio.setmana2.repositori.RepositoriEscola;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(rollbackFor={EdatIncorrecteException.class})
 public class ServeiEscola {
 
-	
+	@Autowired
+	RepositoriEscola repositori;
 	/**
 	 * Important: els alumnes i els cursos indicats JA existeixen a la base de dades.
 	 * Per tant, els hem de carregar, no crear de nou.
 	 * L'excepcio EdatIncorrecteException no s'ha de capturar. S'ha de propagar cap el client
 	 */
 	public List<Matricula> apunta (String curs, List<String> alumnes) throws EdatIncorrecteException {
-		return null;
+		List<Matricula> matricules = new ArrayList<Matricula>();
+		for (String alumne: alumnes) {
+			matricules.add(repositori.apunta(alumne, curs));
+		}
+		return matricules;
     }
 }
